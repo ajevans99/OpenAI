0d17db79f0649e115c9053275565c4c5cd063450
Austin Evans <austinjevans@me.com>
2024-12-14 17:02:16 -0500

    Use JSONValue object

--- Sources/OpenAI/Public/Models/ChatQuery.swift
+++ Sources/OpenAI/Public/Models/ChatQuery.swift
@@ -674,12 +674,12 @@ public struct ChatQuery: Equatable, Codable, Streamable {
             /// https://platform.openai.com/docs/guides/text-generation/function-calling
             /// https://json-schema.org/understanding-json-schema/
             /// **Python library defines only [String: Object] dictionary.
-            public let parameters: String?
+            public let parameters: [String: JSONValue]?
 
             public init(
                 name: String,
                 description: String? = nil,
-                parameters: String? = nil
+                parameters: [String: JSONValue]? = nil
             ) {
                 self.name = name
                 self.description = description
@@ -712,4 +712,79 @@ public struct ChatQuery: Equatable, Codable, Streamable {
         case user
         case stream
     }
+}
+
+public enum JSONValue: Hashable, Equatable, Sendable {
+  case string(String)
+  case number(Double)
+  case integer(Int)
+  case object([String: Self])
+  case array([Self])
+  case boolean(Bool)
+  case null
+
+  public static func == (lhs: JSONValue, rhs: JSONValue) -> Bool {
+    switch (lhs, rhs) {
+    case (.string(let lhsValue), .string(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.number(let lhsValue), .number(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.integer(let lhsValue), .integer(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.number(let lhsValue), .integer(let rhsValue)):
+      return lhsValue == Double(rhsValue)
+    case (.integer(let lhsValue), .number(let rhsValue)):
+      return Double(lhsValue) == rhsValue
+    case (.object(let lhsValue), .object(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.array(let lhsValue), .array(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.boolean(let lhsValue), .boolean(let rhsValue)):
+      return lhsValue == rhsValue
+    case (.null, .null):
+      return true
+    default:
+      return false
+    }
+  }
+}
+
+extension JSONValue: Codable {
+  public func encode(to encoder: any Encoder) throws {
+    var container = encoder.singleValueContainer()
+    switch self {
+    case .string(let string): try container.encode(string)
+    case .number(let double): try container.encode(double)
+    case .integer(let int): try container.encode(int)
+    case .object(let dictionary): try container.encode(dictionary)
+    case .array(let array): try container.encode(array)
+    case .boolean(let bool): try container.encode(bool)
+    case .null: try container.encodeNil()
+    }
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if let string = try? container.decode(String.self) {
+      self = .string(string)
+    } else if let int = try? container.decode(Int.self) {
+      // It is important to check for integer before double, as all integers are also doubles.
+      self = .integer(int)
+    } else if let double = try? container.decode(Double.self) {
+      self = .number(double)
+    } else if let dictionary = try? container.decode([String: Self].self) {
+      self = .object(dictionary)
+    } else if let array = try? container.decode([Self].self) {
+      self = .array(array)
+    } else if let bool = try? container.decode(Bool.self) {
+      self = .boolean(bool)
+    } else if container.decodeNil() {
+      self = .null
+    } else {
+      throw DecodingError.dataCorruptedError(
+        in: container,
+        debugDescription: "Unrecognized JSON value"
+      )
+    }
+  }
 }
